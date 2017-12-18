bfa0342198137ae36bf7946c2b241cdecec50cd1
MisanthropicBit <albo.developer@gmail.com>
2017-12-18 23:28:54 +0100

    Add chili plant configuration

---
+++ pyrigate/configs/chili.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Watering settings for chili plants."""
+
+configuration = {
+    'name':      'chili',
+    'scheme':    'auto',
+    'amount':    '0.1dl',
+    'frequency': 2,
+    'per':       'day',
+    'times':     ['09:00', '20:00']
+}
