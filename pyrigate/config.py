c1bff6a1c53c330e9e501b1a66bce03713d2f91a
MisanthropicBit <albo.developer@gmail.com>
2018-10-08 17:33:54 +0200

    Clean up config class, move config dir and switch to json format

--- pyrigate/config.py
+++ pyrigate/config.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Generic configuration class for watering and settings."""
+"""Generic plant configuration class."""
 
 import functools
-import importlib
-import pyrigate
-import settings
-import default_settings
+import json
+from pyrigate.logging import error
 
 
 def configurable(config, setting):
@@ -24,56 +22,52 @@ def configurable(config, setting):
 
 
 class Configuration(object):
-    """Light-weight configuration class.
+    """Light-weight configuration class for a specific plant.
 
     Its main purpose is to provide sensible defaults when certain setting are
     not found.
 
     """
 
-    _DEFAULT_CONFIG = {
-        'name':      'unknown',
-        'amount':    0,
-        'frequency': 0,
-        'per':       'unknown',
-    }
-
     def __init__(self, filename=''):
+        """Initialise a configuration, optionally reading from a file."""
+        self._mapping = {}
+
         if filename:
             self.load(filename)
 
+    @classmethod
+    def required(cls):
+        """Set of required configuration keys."""
+        return frozenset([
+            'name',
+            'amount',
+            'frequency',
+            'per'
+        ])
+
+    @classmethod
+    def extension(cls):
+        """File extension of configuration files."""
+        return 'json'
+
     def load(self, filename):
         """Load a configuration file."""
-        config = importlib.import_module(filename)
+        with open(filename) as fh:
+            self._config = json.load(fh)
 
-        if not hasattr(config, 'configuration'):
-            pyrigate.error(AttributeError, "'{0}' did not have any settings",
-                           filename)
-
-        if 'name' not in config.configuration:
-            pyrigate.error(AttributeError,
-                           "'{0}' does not have a required name", filename)
-
-        self._set_values(config.configuration, Configuration._DEFAULT_CONFIG)
+            for key in Configuration.required():
+                if key not in self._config:
+                    error(AttributeError,
+                          "'{0}' does not have required configuration key "
+                          "'{1}'", filename, key)
 
     def __getitem__(self, key):
-        return self._mapping[key]
+        return self._config[key]
 
     def __setitem__(self, key, value):
-        self._mapping[key] = value
-
-    def _set_values(self, config, defaults):
-        self._mapping = {}
-
-        for attr in defaults:
-            self._mapping[attr] = config.get(attr, defaults[attr])
+        self._config[key] = value
 
     def list(self):
-        print("{0:<20} {1:>20}".format('Name', self._mapping['name']))
-
-        for k, v in self._mapping.items():
-            if k != 'name':
-                print("{0:<20} {1:>20}".format(k.title(), v))
-
         for k, v in self._config.items():
             print("{0:<20} {1:>20}".format(k, v))
