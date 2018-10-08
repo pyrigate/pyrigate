14105ff6f8ad4c8eec4e485a40d1cec86b1a1e0c
MisanthropicBit <albo.developer@gmail.com>
2018-10-08 17:12:24 +0200

    Update setup.py
    Add an entry point so users can call 'pyrigate' on the command line.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-"""pyrigate distribution script."""
-
-from __future__ import with_statement
-
 import os
-import distutils.core
+from setuptools import setup, find_packages
+
+
+def long_description():
+    with open('README.md') as readme_file:
+        return readme_file.read()
 
 
 def get_version(filename):
@@ -13,27 +14,40 @@ def get_version(filename):
                 return line.split('=')[-1].strip()[1:-1]
 
 
-distutils.core.setup(
+requirements = [
+    'docopt',
+    'schedule'
+]
+
+setup(
     name='pyrigate',
     version=get_version(os.path.join('pyrigate', '__init__.py')),
     author='Alexander Asp Bock',
     author_email='albo.developer@gmail.com',
     platforms='All',
     description=('Single-plant automated and configurable watering system'),
+    long_description=long_description(),
+    python_requires='>=3.4',
     license='MIT',
-    keywords='Raspberry pi, watering, automated',
+    keywords='Raspberry pi, rpi, watering, automated',
     url='https://github.com/MisanthropicBit/pyrigate',
-    packages=['pyrigate', 'pyrigate.sensors'],
-    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=requirements,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Utilities',
         'Topic :: Software Development',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
-    ]
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyrigate = pyrigate.main:main'
+        ]
+    }
 )
