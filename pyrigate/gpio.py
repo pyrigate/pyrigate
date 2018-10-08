21e8bfea425a991c6d40bd37a3674071fcdcea2b
MisanthropicBit <albo.developer@gmail.com>
2018-10-08 17:19:40 +0200

    Add gpio module interface
    When not running on a raspberry pi, the module fakes gpio no-op
    functions so we can still call them.

---
+++ pyrigate/gpio.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+"""GPIO control functions.
+
+Mocks all functions (no-ops) if we are not running on a raspberry pi. See
+https://sourceforge.net/projects/raspberry-gpio-python/ for details.
+
+"""
+
+try:
+    from RPi.GPIO import *
+
+    def mocked():
+        """Return True if gpio functions are being mocked."""
+        return False
+except ImportError:
+    def mocked():
+        """Return True if gpio functions are being mocked."""
+        return True
+
+    # Global variables
+    SETUP_OK = 0
+    SETUP_DEVMEM_FAIL = 1
+    SETUP_MALLOC_FAIL = 2
+    SETUP_MMAP_FAIL = 3
+    SETUP_CPUINFO_FAIL = 4
+    SETUP_NOT_RPI_FAIL = 5
+
+    INPUT = 1
+    OUTPUT = 0
+    ALT0 = 4
+
+    HIGH = 1
+    LOW = 0
+
+    PUD_OFF = 0
+    PUD_DOWN = 1
+    PUD_UP = 2
+
+    def setup(*args, **kwargs):
+        pass
+
+    def cleanup(*args, **kwargs):
+        pass
+
+    def output(*args):
+        pass
+
+    def input(*args):
+        pass
+
+    def setmode(*args):
+        pass
+
+    def getmode(*args):
+        pass
+
+    def add_event_detect(*args, **kwargs):
+        pass
+
+    def remote_event_detect(*args):
+        pass
+
+    def event_detected(*args):
+        return False
+
+    def add_event_callback(*args, **kwargs):
+        pass
+
+    def wait_for_edge(*args, **kwargs):
+        pass
+
+    def gpio_function(*args):
+        pass
+
+    def setwarnings(*args):
+        pass
