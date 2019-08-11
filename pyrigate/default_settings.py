7b5eaef61395d40ac960db6f5ef86c46f4e367c0
MisanthropicBit <albo.developer@gmail.com>
2019-08-11 15:14:15 +0200

    Update default settings

--- pyrigate/default_settings.py
+++ pyrigate/default_settings.py
@@ -3,9 +3,11 @@
 
 """pyrigate default settings."""
 
+#####################################################################
+# !!! USERS SHOULD MODIFY 'user_settings.py' INSTEAD OF THIS FILE !!!
+#####################################################################
 
-# USERS SHOULD MODIFY 'settings.py' INSTEAD OF THIS FILE
-settings = {
+defaults = {
     # Prefix used for pyrigate console output
     'prefix': '💦 🌱 ',
 
@@ -16,23 +18,28 @@ settings = {
     'logging': False,
 
     # Logging format. Default displays as:
-    # '[2006-02-08 22:20:02] INFO: Error message (pyrigate.function)'
-    'log_format': '[%(asctime)s] %(levelname)s: %(message)s '
-                  '(%(module)s.%(funcName)s)',
+    # '[2006-02-08 22:20:02] INFO: This is the error message'
+    'log_format': '[%(asctime)s] %(levelname)s: %(message)s ',
 
-    # Logging directory
+    # Logging directory relative to the parent of pyrigate's top-level package
     'log_dir': './logs',
 
-    # Send a warning when water levels are below this level (in deciliter)
-    'warn_at_water_level': 0.1,
+    # Send a warning when water levels are below this level
+    'warn_at_water_level': '0.1dl',
 
-    # If True, sends status updates each week to the addresses listed in
-    # email.subscribers
+    # If True, send status updates to the addresses listed in email.subscribers
+    # as dictated by 'status_frequency'
     'status_updates': True,
+
+    # Send status updates with this frequency
+    'status_frequency': 'weekly',
 
     # Email subconfiguration
     'email': {
-        # Send a warning email about low water levels to these emails
+        # The mail to send notifications from
+        'sender': '',
+
+        # Send notifications to this list of subscribers
         'subscribers': [],
 
         # SMTP server to use
@@ -45,12 +52,26 @@ settings = {
         'use_ssl': True
     },
 
-    # A list of all connected pumps. Requires at least a specified connecting
-    # GPIO pin and a flow rate
+    # A list of all connected pumps. Requires at least specifying the gpio
+    # output pin and flow rate
     'pumps': {
         'main': {
             'pin': 7,
             'flow_rate': '1.2L/min'
+        }
+    },
+
+    # A list of all connected sensors. Requires at least specifying the gpio
+    # input pin and analog/digital. The threshold can be specified if the
+    # sensor needs to trigger some action when it is crossed.
+    #
+    # Only moisture sensors and the 'water' action are currently supported.
+    'sensors': {
+        'moisture-sensor': {
+            'pin': 5,
+            'threshold': 0.1,
+            'trigger': 'water',
+            'analog': False
         }
     }
 }
