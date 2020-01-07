5b10bb73a1d8b93afa1502024ae6e9874ba1f932
MisanthropicBit <albo.developer@gmail.com>
2020-01-07 15:37:09 +0100

    Use Python 3 super() syntax

--- pyrigate/sensors/water_level.py
+++ pyrigate/sensors/water_level.py
@@ -9,7 +9,7 @@ class WaterLevelSensor(Sensor):
     """Water-level sensor controller class."""
 
     def __init__(self, pin, threshold, analog):
-        super(WaterLevelSensor, self).__init__(pin, threshold, analog)
+        super().__init__(pin, threshold, analog)
 
     def read(self):
         return 0
