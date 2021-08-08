d35898da5d2fd5970a8b5f77dc440300a24f7f8c
MisanthropicBit <albo.developer@gmail.com>
2021-08-08 10:20:56 +0200

    Refactor job scheduling, parse units for pump

--- pyrigate/jobs/watering_job.py
+++ pyrigate/jobs/watering_job.py
@@ -5,59 +5,60 @@
 
 import schedule
 from pyrigate.jobs import Job
+from pyrigate.units.parse import parse_unit
 
 
 class WateringJob(Job):
     """A pyrigate watering job."""
 
-    def __init__(self, config=None):
+    def __init__(self, controller, config=None):
         super().__init__()
         self._description = ''
+        self._controller = controller
 
         if config:
             self.schedule(config)
 
     def schedule(self, config):
         """Schedule a watering job from a plant configuration."""
-        descriptions = []
+        self._config = config
         scheme = config.scheme
         amount = scheme['amount']
 
         for when in scheme['when']:
             job = None
-            description = []
 
             if 'on' in when:
-                job = getattr(schedule.every(), when['on'])
-                description.append('on ' + when['on'] + 's at')
+                job = self._schedule_on_type(when['on'])
             else:
-                each = when['each']
-                description.append('each ' + each + ' at')
-
-                if each == 'month':
-                    job = schedule.every(interval=4).weeks
-                elif each == 'year':
-                    job = schedule.every(interval=52).weeks
-                else:
-                    job = getattr(schedule.every(), when['each'])
+                job = self._schedule_each_type(when['each'])
 
             for time in when['at']:
                 job = getattr(job, 'at')(time)
-                description.append(time)
 
             job.do(self.task, amount)
-            descriptions.append(' '.join(description))
 
-        self._description = ' and '.join(descriptions)
+    def _schedule_on_type(self, on):
+        return getattr(schedule.every(), on)
 
-    def task(self, amount):
-        print('Watering {0}'.format(amount))
-        self._runs += 1
-        # self.pump.pump(amount)
+    def _schedule_each_type(self, each):
+        if each == 'month':
+            return schedule.every(interval=4).weeks
+        elif each == 'year':
+            return schedule.every(interval=52).weeks
+        else:
+            return getattr(schedule.every(), each)
+
+    def task(self, amount_string):
+        pump = self._controller.get_pump(self._config.scheme['pump'])
+
+        if pump:
+            self._runs += 1
+            pump.pump_unit(*parse_unit(amount_string))
 
     @property
     def description(self):
-        return self._description
+        return self._config.schedule_description
 
     @property
     def type(self):
